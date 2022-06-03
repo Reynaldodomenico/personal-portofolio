8b13e688c2b54f253cbe693878902774281fc247
Reynaldo Domenico <reynaldodomenico@yahoo.com>
2022-06-03 18:32:48 +0200

    finish the Home page

--- js/script.js
+++ js/script.js
@@ -3,10 +3,19 @@ let header = document.querySelector("header");
 
 menu.onclick = () => {
   menu.classList.toggle("fa-times");
-  header.classList.toggle("active");
+  header.classList.toggle("active1");
 };
 
 window.onscroll = () => {
   menu.classList.remove("fa-times");
   header.classList.remove("active");
 };
+
+(function () {
+  [...document.querySelectorAll(".control")].forEach((button) => {
+    button.addEventListener("click", function () {
+      document.querySelector(".active").classList.remove("active");
+      document.getElementById(button.dataset.id).classList.add("active");
+    });
+  });
+})();
