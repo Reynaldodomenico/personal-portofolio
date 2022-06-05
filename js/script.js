71df278dfb869a78ecbad38cdf247ee29a4e97dd
Reynaldo Domenico <reynaldodomenico@yahoo.com>
2022-06-05 21:05:45 +0200

    fixed menu bar bug

--- js/script.js
+++ js/script.js
@@ -7,22 +7,12 @@ menu.onclick = () => {
   header.classList.toggle("active");
 };
 
-window.onscroll = () => {
-  menu.classList.remove("fa-times");
-  header.classList.remove("active");
-};
-
-// buttoncontact.onclick = () => {
-//   classList.remove("activechild");
-//   document.getElementById(dataset.id).classList.add("activechild");
-// };
-
 (function () {
   [...document.querySelectorAll(".control")].forEach((button) => {
     button.addEventListener("click", function () {
       document.querySelector(".activechild").classList.remove("activechild");
       document.getElementById(button.dataset.id).classList.add("activechild");
-      classList.remove(menu);
+      header.classList.remove("active");
     });
   });
 })();
