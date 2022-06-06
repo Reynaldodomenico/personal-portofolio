7c72b3d5557eca32a05e260e9b2b158f15aa6043
Reynaldo Domenico <reynaldodomenico@yahoo.com>
2022-06-06 22:55:04 +0200

    edited script.js

--- js/script.js
+++ js/script.js
@@ -1,19 +1,14 @@
 let menu = document.querySelector("#menu-bars");
 let header = document.querySelector("header");
-let buttoncontact = document.querySelector(".btn-contact");
+let control = document.querySelectorAll(".control");
 
 menu.onclick = () => {
   menu.classList.toggle("fa-times");
   header.classList.toggle("active");
 };
 
-(function () {
-  [...document.querySelectorAll(".control")].forEach((button) => {
-    button.addEventListener("click", function () {
-      document.querySelector(".activechild").classList.remove("activechild");
-      document.getElementById(button.dataset.id).classList.add("activechild");
-      menu.classList.toggle("fa-times");
-      header.classList.remove("active");
-    });
-  });
-})();
+control.onclick = () => {
+  document.querySelector(".activechild").classList.remove("activechild");
+  document.getElementById(button.dataset.id).classList.add("activechild");
+  header.classList.remove("active");
+};
